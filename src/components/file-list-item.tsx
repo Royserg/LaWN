5ba4f2a44380311dfe7e6d401f360629331ced8d
Royserg <staniojakub@gmail.com>
2023-10-21 00:57:49 +0300

    refactor(FileListItem): split Directory and File to separate components

--- src/components/file-list-item.tsx
+++ src/components/file-list-item.tsx
@@ -10,6 +10,22 @@ interface FileListItemProps {
   data: FileOrDirectoryType;
 }
 export const FileListItem: FC<FileListItemProps> = ({ data }) => {
+  if (data.kind === "directory") {
+    return <ItemDirectory dirHandle={data} />;
+  }
+
+  if (data.kind === "file") {
+    return <ItemFile fileHandle={data} />;
+  }
+};
+FileListItem.displayName = "FileListItem";
+
+// Item Directory
+
+interface ItemDirectoryProps {
+  dirHandle: FileSystemDirectoryHandle;
+}
+const ItemDirectory: FC<ItemDirectoryProps> = ({ dirHandle }) => {
   const getDirFiles = useFileStore((state) => state.getDirectoryFiles);
   const [pressed, setPressed] = useState(false);
   const [files, setFiles] = useState<FileOrDirectoryType[]>([]);
@@ -17,7 +33,7 @@ export const FileListItem: FC<FileListItemProps> = ({ data }) => {
   const handleDirectoryClick = async (isPressed: boolean) => {
     setPressed(isPressed);
     if (isPressed) {
-      const dirFiles = await getDirFiles(data as FileSystemDirectoryHandle);
+      const dirFiles = await getDirFiles(dirHandle);
       if (dirFiles) {
         setFiles(dirFiles);
       }
@@ -27,10 +43,9 @@ export const FileListItem: FC<FileListItemProps> = ({ data }) => {
       setFiles([]);
     }
   };
-
-  const content = () => {
-    if (data.kind === "directory") {
-      return (
+  return (
+    <>
+      <div className="pl-1 cursor-pointer flex justify-start items-center gap-2 hover:bg-slate-100 rounded-md transition-colors">
         <Toggle
           className="flex justify-start items-center w-full gap-2 hover:bg-slate-100 p-0"
           onPressedChange={handleDirectoryClick}
@@ -41,25 +56,8 @@ export const FileListItem: FC<FileListItemProps> = ({ data }) => {
           ) : (
             <Folder className="w-4" />
           )}
-          {data.name}
+          {dirHandle.name}
         </Toggle>
-      );
-    }
-
-    if (data.kind === "file") {
-      return (
-        <>
-          <File className="w-4" />
-          {data.name}
-        </>
-      );
-    }
-  };
-
-  return (
-    <>
-      <div className="pl-1 cursor-pointer flex justify-start items-center gap-2 hover:bg-slate-100 rounded-md transition-colors">
-        {content()}
       </div>
       {files && files.length > 0 && (
         <FileList files={files} className="text-[10px] pl-1" />
@@ -67,4 +65,17 @@ export const FileListItem: FC<FileListItemProps> = ({ data }) => {
     </>
   );
 };
-FileListItem.displayName = "FileListItem";
+
+// Item File
+
+interface ItemFileProps {
+  fileHandle: FileSystemHandle;
+}
+const ItemFile: FC<ItemFileProps> = ({ fileHandle }) => {
+  return (
+    <div className="pl-1 cursor-pointer flex justify-start items-center gap-2 hover:bg-slate-100 rounded-md transition-colors">
+      <File className="w-4" />
+      {fileHandle.name}
+    </div>
+  );
+};
