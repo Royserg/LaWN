a765298fa69fd0ed327ddb0d18252a6d0ed0f727
Royserg <staniojakub@gmail.com>
2023-10-21 23:22:48 +0300

    chore:reuse getDirFiles, add selectedFile field

--- src/store/file.store.ts
+++ src/store/file.store.ts
@@ -12,69 +12,69 @@ const persistanceKeys = {
 type FileStore = {
   rootDirHandle: FileSystemDirectoryHandle | null;
   rootFiles: FileOrDirectoryType[];
+  selectedFile: FileSystemFileHandle | null;
 
   getDirectoryFiles: (
     dirHandle: FileSystemDirectoryHandle,
   ) => Promise<FileOrDirectoryType[] | undefined>;
   pickRootDirectory: () => Promise<void>;
+  selectFile: (fileHandle: FileSystemFileHandle) => void;
 
   initializeStore: () => Promise<void>;
 };
 
-export const useFileStore = create<FileStore>()(
-  // persist(
-  (set, get) => ({
-    rootDirHandle: null,
-    rootFiles: [],
+export const useFileStore = create<FileStore>()((set, get) => ({
+  rootDirHandle: null,
+  rootFiles: [],
+  selectedFile: null,
 
-    pickRootDirectory: async () => {
-      let handle: FileSystemDirectoryHandle;
+  pickRootDirectory: async () => {
+    let handle: FileSystemDirectoryHandle;
 
-      try {
-        handle = await window.showDirectoryPicker({ mode: "readwrite" });
-
-        const files: FileOrDirectoryType[] = [];
-        for await (const fileOrDir of handle.values()) {
-          if (!fileOrDir.name.startsWith(".")) {
-            files.push(fileOrDir);
-          }
-        }
-
-        set({
-          rootDirHandle: handle,
-          rootFiles: files,
-        });
-
-        await idbSet(persistanceKeys.rootDirHandle, handle);
-      } catch (err) {
-        if (err instanceof DOMException) {
-          console.log("User canceled directory select prompt");
-        } else {
-          console.log("ERROR:", err);
-        }
-      }
-    },
-
-    getDirectoryFiles: async (dirHandle: FileSystemDirectoryHandle) => {
-      try {
-        const files: FileOrDirectoryType[] = [];
-        for await (const fileOrDir of dirHandle.values()) {
-          files.push(fileOrDir);
-        }
-        return files;
-      } catch (err) {
-        console.log("Reading directory files error:", err);
-      }
-    },
-
-    initializeStore: async () => {
-      const rootDirHandle = await idbGet(persistanceKeys.rootDirHandle);
-      const files = await get().getDirectoryFiles(rootDirHandle);
+    try {
+      handle = await window.showDirectoryPicker({ mode: "readwrite" });
+      const files = await get().getDirectoryFiles(handle);
 
       set({
-        rootDirHandle: rootDirHandle,
-        rootFiles: files || [],
+        rootDirHandle: handle,
+        rootFiles: files,
       });
-    },
-  }),
-);
+
+      await idbSet(persistanceKeys.rootDirHandle, handle);
+    } catch (err) {
+      if (err instanceof DOMException) {
+        console.log("User canceled directory select prompt");
+      } else {
+        console.log("ERROR:", err);
+      }
+    }
+  },
+
+  getDirectoryFiles: async (dirHandle: FileSystemDirectoryHandle) => {
+    try {
+      const files: FileOrDirectoryType[] = [];
+      for await (const fileOrDir of dirHandle.values()) {
+        if (!fileOrDir.name.startsWith(".")) {
+          files.push(fileOrDir);
+        }
+      }
+      return files;
+    } catch (err) {
+      console.log("Reading directory files error:", err);
+    }
+  },
+
+  selectFile: (fileHandle) => {
+    set({ selectedFile: fileHandle });
+  },
+
+  initializeStore: async () => {
+    const rootDirHandle = await idbGet(persistanceKeys.rootDirHandle);
+    const files = await get().getDirectoryFiles(rootDirHandle);
+
+    set({
+      rootDirHandle: rootDirHandle,
+      rootFiles: files || [],
+    });
+  },
+}));
